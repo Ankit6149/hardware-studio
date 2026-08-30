0ad8690f2f51ef289efbc0b9fb70bdb688af44da
Ankit Bhardwaj <97785108+Ankit6149@users.noreply.github.com>
2026-08-30 07:42:41 -0700

    feat(routing): serve clean nested Studio routes

---
+++ src/app/studio/[[...path]]/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const StudioRoot = dynamic(
+  () => import('../../../components/reliability/StudioRoot').then((mod) => mod.StudioRoot),
+  { ssr: false },
+);
+
+export default function StudioPage() {
+  return <StudioRoot />;
+}
